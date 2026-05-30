841b024340651fa701eaf805cd2cc318d97a8fa0
rokeyaag <lutforitsolution@gmail.com>
2026-05-31 01:25:12 +0600

    Added About, Skills, Projects and Contact pages

---
+++ js/main.js
@@ -0,0 +1,36 @@
+*{
+  margin:0;
+  padding:0;
+  box-sizing:border-box;
+}
+
+html{
+  scroll-behavior:smooth;
+}
+
+body{
+
+  font-family:'Poppins',sans-serif;
+
+  background:#020617;
+
+  color:#fff;
+
+  overflow-x:hidden;
+}
+
+.container{
+
+  width:90%;
+
+  max-width:1400px;
+
+  margin:auto;
+}
+
+img{
+
+  max-width:100%;
+
+  display:block;
+}
